d5bf59b66e93d447db5a8927c8b857741ce67227
IBrickhouse <mrfurbyb@gmail.com>
2023-08-19 19:14:11 -0700

    Delete vent functionality added

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ async function greet() {
 
 function post() {
     if (document.getElementById("shareButton").checked) {
-        console.log("Checked")
+        // console.log(document.getElementById("titleOfPost").value," : ", document.getElementById("contentOfPost").value, ".", " Sipping on some ", document.getElementById("beverage").value)
+        window.alert("thanks for venting. you can view your vent at ")
     }
     else {
-        console.log("Not Checked")
-        location.reload();
+        // console.log("Not Checked")
+        doneButton.innerHTML = "Thanks for venting. reloading page in 3 seconds..."
+        setTimeout(deleteVent, 3000)
     }
+}
+
+function deleteVent() {
+    location.reload();
 }
