32e7c08d99720e8195d44379da346d7899d452d7
IBrickhouse <mrfurbyb@gmail.com>
2023-11-01 00:49:11 -0700

    Implement food choice
    Implement food choice that allows the user to input what they are currently eating. if they say yes, it will add a text box to allow them to say what. if they choose no then it will suggest something to eat. plan to do a similar thing to the beverage choice. there is a bug here that creates too many text boxes if the user goes back and forth on the selection. will fix later

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ function postCheck() {
 function deleteVent() {
     location.reload();
 }
+
+var foodSelect = document.getElementById("food")
+
+foodSelect.onchange = function foodSuggestion() {
+    var foodChoice = document.getElementById("food").value;
+
+    if (foodChoice == "no") {
+
+        const noFood = document.createElement("p");
+        noFood.innerHTML = "You Should eat something. How about some toast?";
+        document.getElementById("myFood").appendChild(noFood);
+    }
+    else {
+        const whatFood = document.createElement("textarea")
+        whatFood.className = 'form-control';
+        //whatFood.setAttribute =("id", "snack")
+        whatFood.innerHTML = "";
+        document.getElementById("myFood").appendChild(whatFood);
+        //console.log(document.getElementById("snack"))
+    }
+}
