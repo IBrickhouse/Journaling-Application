1e2ea9e323c293abbd26bef8bdc500562dbf8995
IBrickhouse <mrfurbyb@gmail.com>
2023-11-03 22:01:50 -0700

    Retry addition of meta tags to optimize iOS web app

--- index.js
+++ index.js
@@ -57,9 +57,3 @@ drinkSelect.onchange = function drinkSuggestion() {
         //console.log(document.getElementById("snack"))
     }
 }
-
-// if ('standalone' in window.navigator && window.navigator.standalone){ //checks if you're in app mode
-
-//     window.location = 'https://symphonious-duckanoo-c15d14.netlify.app/';  //the URL you want to refer to
-  
-// }
